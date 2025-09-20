bfc739611976d06304cca276b10a1fe9aeb76c34
lucchettan <45241293+lucchettan@users.noreply.github.com>
2025-09-20 20:54:49 +0200

    fix: Adjust stroke for Bars3Icon
    Corrected the `d` attribute for the `Bars3Icon` SVG to properly render the middle line with the intended offset.

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -348,6 +348,6 @@ export const ShieldCheckIcon: React.FC<IconProps> = ({ className = 'w-6 h-6', ..
 
 export const Bars3Icon: React.FC<IconProps> = ({ className = 'w-6 h-6', ...rest }) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className} {...rest}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-1.5 5.25h16.5" />
   </svg>
 );
